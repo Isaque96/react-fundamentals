07fc3e83f35ee620bada0e02392b2195cc296929
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2022-04-12 19:44:21 -0300

    2 classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-/* eslint-disable import/no-anonymous-default-export */
 import React from "react";
 
-import First from "./components/basics/First";
-import WithParameter from "./components/basics/WithParameter";
+import Card from "./components/layout/Card";
+import Random from "./components/basics/Random";
 import Fragment from "./components/basics/Fragment";
+import WithParameter from "./components/basics/WithParameter";
+import First from "./components/basics/First";
 
-export default (_) => (
-  <div>
-    <h1>Exercícios Fundamentos</h1>
-    <Fragment />
-    <WithParameter titulo="Segundo Componente" aluno="Isaque" nota={9.8} />
-    <First />
-  </div>
-);
+export default function App(_) {
+  return (
+    <div id="app">
+      <h1>Exercícios Fundamentos React</h1>
+      <Card titulo="Desafio Aleatório">
+        <Random min={1} max={60} />
+      </Card>
+      <Card titulo="Fragmento">
+        <Fragment />
+      </Card>
+      <Card titulo="Com Parâmetro">
+        <WithParameter titulo="Segundo Componente" aluno="Isaque" nota={9.8} />
+      </Card>
+      <Card titulo="Primeiro">
+        <First />
+      </Card>
+    </div>
+  );
+}
